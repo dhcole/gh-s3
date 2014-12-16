3335533e421abbd798ddc2790029b7095a456017
Dave Cole <dave@developmentseed.org>
2014-12-16 17:09:37 -0500

    Use script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+sudo apt-get update
+sudo apt-get install git
+gem install github-pages -v 28
+gem install jekyll-assets -v 0.11.0
+gem install react-jsx-sprockets: -v 0.2.0
+
 mkdir ~/static
 cd ~/static
 git init
