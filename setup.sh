2466c02dd5e189fc81f0eb9fa090f1cc5a1f244d
Dave Cole <dave@developmentseed.org>
2014-12-16 17:30:16 -0500

    Update script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install git
 gem install github-pages -v 28
 gem install jekyll-assets -v 0.11.0
-gem install react-jsx-sprockets: -v 0.2.0
+gem install react-jsx-sprockets -v 0.2.0
 
 mkdir ~/static
 cd ~/static
